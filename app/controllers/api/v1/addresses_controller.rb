2d12fbb59b86a20af6389fb37ac4d918a19c7998
MarceloAGuimaraes <marceloguimaraesti@gmail.com>
2021-03-20 15:42:31 -0300

    define namespaces on addresses controller and fix the way received params

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,33 +1,37 @@
-class AddressesController < ApplicationController
-  before_actio :set_services, only: [:coordinates]
+module Api
+  module V1
+    class AddressesController < ApplicationController
+      before_action :set_services, only: [:coordinates]
 
-  def coordinates
-    filter = params[:addres]
-    @result = @google_service.search_coordinate(filter)
-    if @result[:success]
-      sucess_with_google
-    else
-      error_with_google
+      def coordinates
+        filter = params[:address]
+        @result = @google_service.search_coordinate(filter)
+        if @result[:success]
+          sucess_with_google
+        else
+          error_with_google
+        end
+        render json: { body: @body }, status: @status
+      end
+
+      private
+
+      def sucess_with_google
+        @sistema_um_service.update_address(@result[:lat], @result[:long])
+        @status = 200
+        @body = 'Sucesso na integração'
+      end
+
+      def error_with_google
+        @status = 404
+        @body = 'Erro na integração com a api do google'
+      end
+
+      def set_services
+        @google_service = GoogleApiService.new
+        request_id = params[:request_id]
+        @sistema_um_service = SistemaUmService.new("api/v1/set_address_request_coordinates/#{request_id}")
+      end
     end
-    render json: { body: @body }, status: @status
-  end
-
-  private
-
-  def sucess_with_google
-    @sistema_um_service.update_address(@result[:lat], @result[:long])
-    @status = 200
-    @body = 'Sucesso na integração'
-  end
-
-  def error_with_google
-    @status = 404
-    @body = 'Erro na integração com a api do google'
-  end
-
-  def set_services
-    @google_service = GoogleApiService.new
-    request_id = params[:request_id]
-    @sistema_um_service = SistemaUmService.new("api/v1/set_address_request_coordinates/#{request_id}")
   end
 end
