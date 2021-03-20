cc303c8284e417e235fe7d2c6db786c7a311f8e2
MarceloAGuimaraes <marceloguimaraesti@gmail.com>
2021-03-20 15:41:51 -0300

    define namespaces to sistema-dois endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'address_coordinates', to: 'addresses#coordinates'
+  namespace :api do
+    namespace :v1 do
+      get 'address_coordinates', to: 'addresses#coordinates'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
